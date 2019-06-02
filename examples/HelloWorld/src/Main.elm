c886e7a250af04565a142ec49543d175b7ebf1d2
Aaron White <aaron@singularity.vc>
2019-06-01 20:02:23 -0400

    Upgrade aframe

--- examples/HelloWorld/src/Main.elm
+++ examples/HelloWorld/src/Main.elm
@@ -13,43 +13,47 @@ import AFrame.Primitives.Attributes
         , width
         , height
         , depth
+        , vrModeUi
         )
 
-
+--  <a-scene background="color: #FAFAFA">
+--       <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" shadow></a-box>
+--       <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" shadow></a-sphere>
+--       <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D" shadow></a-cylinder>
+--       <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" shadow></a-plane>
+--     </a-scene>
 main : Html msg
 main =
     scene
-        []
+        [ vrModeUi True ]
         [ sphere
-            [ position 0 1.25 -1
+            [ position 0 1.25 -5
             , radius 1.25
             , color (rgb255 240 173 0)
             ]
             []
         , box
-            [ position -1 0.5 1
-            , radius 0.5
-            , width 1
-            , height 1.5
-            , depth 1
+            [ position -1 0.5 -3
+            , rotation 0 45 0
             , color (rgb255 127 209 59)
             ]
             []
         , cylinder
-            [ position 1 0.75 1
+            [ position 1 0.75 -3
             , radius 0.5
             , height 1.5
             , color (rgb255 6 181 204)
             ]
             []
         , plane
-            [ rotation -90 0 0
+            [ position 0 0 -4
+            ,    rotation -90 0 0
             , width 4
             , height 4
             , color (rgb255 90 99 120)
             ]
             []
         , sky
-            []
+            [  color (rgb255 1 5 20) ]
             []
         ]
